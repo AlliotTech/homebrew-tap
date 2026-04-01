4e2c19e4ea419c07dc4577f1bc6934854146d3b4
AlliotTech <24980252+AlliotTech@users.noreply.github.com>
2026-04-02 00:01:05 +0800

    feat: update apaste to version 0.0.7

--- Casks/apaste.rb
+++ Casks/apaste.rb
@@ -1,9 +1,9 @@
 cask "apaste" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.0.6"
-  sha256 arm:   "4a00c5b9e1b2a2b3fde1e9a2099806f317e0878cd014b2271d27a4ac5666a8ae",
-       intel: "99b9ea8a4c737c8bb237ee1f4c6348102398191d0c3eadbc4df4705003e20ae9"
+  version "0.0.7"
+  sha256 arm:   "88d26732257d547aa7dd4017c7091c4f071fb6f3962b310efba0e76d87c72a65",
+       intel: "4f987fcf9830ec86487e91705006b4cbe823d0948ce4da9bb685f4297c5befc1"
 
   url "https://github.com/AlliotTech/aPaste/releases/download/v#{version}/aPaste-v#{version}-#{arch}.dmg"
   name "aPaste"
