c8841ccda9203c66cf17e4d8ba427dbe7f5494b4
AlliotTech <24980252+AlliotTech@users.noreply.github.com>
2026-09-07 16:26:48 +0800

    fix: alt-tab-nopro

--- Casks/alt-tab-no-pro.rb
+++ Casks/alt-tab-no-pro.rb
@@ -2,8 +2,7 @@ cask "alt-tab-no-pro" do
   version "11.5.0"
   sha256 "3b803f08159e7fa987dcfb308854d28bfd32fbf2a54b8d3265aabe4739166468"
 
-  url "https://github.com/AlliotTech/alt-tab-macos/releases/download/v#{version}/AltTab-no-pro-#{version}.zip",
-      verified: "github.com/AlliotTech/alt-tab-macos/"
+  url "https://github.com/AlliotTech/alt-tab-macos/releases/download/v#{version}/AltTab-no-pro-#{version}.zip"
   name "AltTab no-pro"
   desc "No-pro local build of AltTab"
   homepage "https://github.com/AlliotTech/alt-tab-macos"
