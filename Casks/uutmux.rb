36e487032ce1237471f9c5c3e582cfaad8b4d213
AlliotTech <24980252+AlliotTech@users.noreply.github.com>
2026-09-22 23:34:40 +0800

    Update UUTmux cask to v0.1.0

--- Casks/uutmux.rb
+++ Casks/uutmux.rb
@@ -1,8 +1,8 @@
 cask "uutmux" do
   arch arm: "arm64", intel: "x86_64"
   version "0.1.0"
-  sha256 arm:   "0000000000000000000000000000000000000000000000000000000000000000",
-         intel: "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 arm:   "a9af33caf8607a5325fa6d33bc3cad3ab1405ab3f8646748f3f3e72fefa58caa",
+         intel: "202acd019ba654b59c5295bfd2d3f3fcc374a1aa64f1d180ccf3876f2e81e92a"
 
   url "https://github.com/AlliotTech/uu-tmux/releases/download/v#{version}/UUTmux-v#{version}-#{arch}.dmg"
   name "UUTmux"
